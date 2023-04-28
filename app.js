045a1d44ec29dfab4222a856c4f61e06cb085e50
Mateusz <matekumateku@gmail.com>
2023-04-28 22:48:58 +0200

    finished quickly but not sure if it is final version

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector(".btn-hero");
+const mainSection = document.querySelector("main");
+const colorSpan = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  random = Math.floor(Math.random() * colors.length);
+  mainSection.style.backgroundColor = colors[random];
+  colorSpan.innerHTML = colors[random];
+});
--- hex.js
+++ hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.querySelector(".btn-hero");
+const mainSection = document.querySelector("main");
+const colorSpan = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  color = "#";
+  for (i = 0; i < 6; i++) {
+    random = Math.floor(Math.random() * hex.length);
+    color += hex[random];
+  }
+  colorSpan.innerHTML = color;
+  mainSection.style.backgroundColor = color;
+});
